83b5feb4cac98818ed34407e4d48b9180f93658c
Andrejs Pavlovics <andrejs.pavlovics@gmail.com>
2023-06-18 16:55:39 +0200

    Enable logging

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ use warp::ws::{Ws, Message, WebSocket};
 mod protocol;
 mod lobby_session;
 
+#[macro_use] extern crate log;
+
 /// The global unique client id counter.
 static NEXT_CLIENT_ID: AtomicUsize = AtomicUsize::new(1);
 
@@ -28,6 +30,7 @@ type Clients = Arc<RwLock<HashMap<ClientId, ClientSender>>>;
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
 
     // Keep track of all connected clients
     let clients = Clients::default();
@@ -47,7 +50,7 @@ async fn main() {
 
 async fn handle_connection(ws: WebSocket, clients: Clients) {
     let client_id = ClientId(NEXT_CLIENT_ID.fetch_add(1, Ordering::Relaxed));
-    println!("Connected client {:?}", client_id);
+    info!("Connected client {:?}", client_id);
 
     let (mut ws_sender, mut ws_receiver) = ws.split();
     let (client_sender, client_receiver) = mpsc::unbounded_channel::<Output>();
@@ -59,11 +62,11 @@ async fn handle_connection(ws: WebSocket, clients: Clients) {
             match serde_json::to_string(&output).map(Message::text) {
                 Ok(message) => {
                     ws_sender.send(message).unwrap_or_else(|e| {
-                        eprintln!("Failed to send WebSocket message to client {:?}: {}", client_id, e);
+                        error!("Failed to send WebSocket message to client {:?}: {}", client_id, e);
                     }).await;
                 }
                 Err(e) => {
-                    eprintln!("Failed to serialize WebSocket message for client {:?}: {}", client_id, e);
+                    error!("Failed to serialize WebSocket message for client {:?}: {}", client_id, e);
                 }
             };
         }
@@ -81,30 +84,29 @@ async fn handle_connection(ws: WebSocket, clients: Clients) {
                                 process(client_id, &client_sender, input).await;
                             }
                             Err(e) => {
-                                eprintln!("Failed to deserialize WebSocket message for client {:?}: {}", client_id, e);
+                                error!("Failed to deserialize WebSocket message for client {:?}: {}", client_id, e);
                             }
                         }
                     }
                     Err(_) => {
-                        eprintln!("Received non-text WebSocket message from client {:?}, ignoring", client_id);
+                        error!("Received non-text WebSocket message from client {:?}, ignoring", client_id);
                     }
                 }
             }
             Err(e) => {
-                eprintln!("Failed to receive WebSocket message from client {:?}: {}", client_id, e);
+                error!("Failed to receive WebSocket message from client {:?}: {}", client_id, e);
                 break;
             }
         };
     }
 
     // TODO Handle client disconnection
-    // TODO Replace console printing with logging
 }
 
 async fn process(client_id: ClientId, client_sender: &ClientSender, input: Input) {
     lobby_session::process(input).output.into_iter().for_each(|output| {
         client_sender.send(output).unwrap_or_else(|e| {
-            eprintln!("Failed to send message for client {:?}: {}", client_id, e);
+            error!("Failed to send message for client {:?}: {}", client_id, e);
         });
     });
 }
